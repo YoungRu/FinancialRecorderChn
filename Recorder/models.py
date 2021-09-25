6357ba3613a765a4f235604e3788ac0361e0992a
YoungRu <young.loo2013@gmail.com>
2021-09-25 13:32:43 +0800

    chn

--- Recorder/models.py
+++ Recorder/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Revenue(models.Model):
-    PriceAmount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    UnitNumber = models.CharField(max_length=10, null=True, blank=True)
-    PayerName = models.CharField(max_length=100, null=True, blank=True)
-    Doc = models.ImageField(null=True, upload_to='proof/',blank=True)
+    PriceAmount = models.DecimalField('数目', max_digits=10, decimal_places=2, null=True)
+    UnitNumber = models.CharField('顾客单元', max_length=10, null=True, blank=True)
+    PayerName = models.CharField('顾客姓名', max_length=100, null=True, blank=True)
+    Doc = models.ImageField('文件', null=True, upload_to='proof/',blank=True)
     Time = models.TimeField(auto_now_add=True)
     Date = models.DateField(auto_now_add=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, default='1')
@@ -15,9 +15,9 @@ class Revenue(models.Model):
         return self.PayerName
 
 class Expend(models.Model):
-    PriceAmount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    Supplier = models.CharField(max_length=50, blank=True)
-    Doc = models.FileField(null=True, upload_to='proof/', blank=True)
+    PriceAmount = models.DecimalField('数目', max_digits=10, decimal_places=2, null=True)
+    Supplier = models.CharField('供货商', max_length=50, blank=True)
+    Doc = models.FileField('文件', null=True, upload_to='proof/', blank=True)
     Time = models.TimeField(auto_now_add=True)
     Date = models.DateField(auto_now_add=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, default='1')
@@ -26,8 +26,8 @@ class Expend(models.Model):
         return str(self.Supplier)
 
 class Labour(models.Model):
-    PriceAmount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    LabourName = models.CharField(max_length=50, blank=True)
+    PriceAmount = models.DecimalField('数目', max_digits=10, decimal_places=2, null=True)
+    LabourName = models.CharField('工人姓名', max_length=50, blank=True)
     Time = models.TimeField(auto_now_add=True)
     Date = models.DateField(auto_now_add=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, default='1')
