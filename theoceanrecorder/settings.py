468dba2624da179ceaf0aac66568ccc6e03be254
YoungRu <young.loo2013@gmail.com>
2021-11-04 00:39:41 +0800

    postgres

--- theoceanrecorder/settings.py
+++ theoceanrecorder/settings.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = 'theoceanrecorder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'financialrecorder',
+        'USER': 'postgres',
+        'PASSWORD': 'Young.loo1999',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
